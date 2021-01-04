ce038da0ed005dab01e491c1e038d533944fd05e
Steve Maya <steve.maya@codeimmersives.com>
2021-01-03 21:43:29 -0600

    commit

--- main.js
+++ main.js
@@ -1,4 +1,57 @@
 // Your code goes below:
+const fizzBuzzSingleNumber = (num) => {
+  if (num % 3 === 0 && num % 5 === 0) {
+    return "FizzBuzz";
+  } else if (num % 3 === 0) {
+    return "Fizz";
+  } else if (num % 5 === 0) {
+    return "Buzz";
+  } else {
+    return num;
+  }
+}
+
+
+const yellAll = (arr) => {
+  const yelled = arr.map(function(str) {
+    return str.toUpperCase();
+  });
+  return yelled;
+}
+
+
+const triStateArea = (cities) => {
+  const triState = cities.filter(function(city) {
+    return city.toLowerCase().endsWith('ny') || city.toLowerCase().endsWith('nj') || city.toLowerCase().endsWith('ct');
+  });
+  return triState;
+};
+
+
+const uppercaseCount = (strs) => {
+  const newArr = [];
+  for (const str of strs) {
+    if(str.toUpperCase() === str) {
+      newArr.push(str);
+    }
+  }
+  return newArr.length;
+}
+
+const containsDigit = (strs) => {
+  const alpha = "abcdefghijklmnopqrstuvwxyz";
+  for (const str of strs) {
+    if(str.includes(alpha)) {
+      return false; 
+    } else if (!str.includes(alpha)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+
 
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
